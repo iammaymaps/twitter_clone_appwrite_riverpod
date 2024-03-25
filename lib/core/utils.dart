b68776dcc808360b4c2ccba7b7cfcb1918812ad5
iammaymaps <iamdeveloper2005@outlook.com>
2024-03-25 16:37:17 +0600

    image picker done

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -11,3 +15,45 @@ void showSnackBar(BuildContext context, String content) {
 String geteNameFromEmail(String email) {
   return email.split('@')[0];
 }
+
+// Future<List<File>> pickImages() async {
+//   List<File> images = [];
+//   final ImagePicker picker = ImagePicker();
+//   final imageFile = await picker.pickMultiImage();
+//   if (imageFile.isNotEmpty) {
+//     for (final image in imageFile) {
+//       images.add(File(image.path));
+//     }
+//   }
+//   return images;
+// }
+
+// Future<File?> pickImage() async {
+//   final ImagePicker picker = ImagePicker();
+//   final imageFile = await picker.pickImage(source: ImageSource.gallery);
+//   if (imageFile != null) {
+//     return File(imageFile.path);
+//   }
+
+//   return null;
+// }
+
+
+   Future<List<File>> pickFiles() async {
+    List<File> files = [];
+    final result = await FilePicker.platform.pickFiles(allowMultiple: true);
+
+    if (result != null) {
+      files = result.paths.map((path) => File(path!)).toList();
+    }
+    return files;
+  }
+
+   Future<File?> pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      return File(result.files.single.path!);
+    }
+    return null;
+  }
--- lib/features/tweet/views/create_tweet_Screen.dart
+++ lib/features/tweet/views/create_tweet_Screen.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:svg_flutter/svg.dart';
 import 'package:twitter_clone_appwrite_riverpod/common/loading_page.dart';
 import 'package:twitter_clone_appwrite_riverpod/common/rounded_small_button.dart';
 import 'package:twitter_clone_appwrite_riverpod/constants/assets_constants.dart';
+import 'package:twitter_clone_appwrite_riverpod/core/utils.dart';
 import 'package:twitter_clone_appwrite_riverpod/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone_appwrite_riverpod/theme/pallete.dart';
 
@@ -18,11 +22,17 @@ class CreateTweetScreen extends ConsumerStatefulWidget {
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
   @override
   final tweetController = TextEditingController();
+  List<File> images = [];
   @override
   void dispose() {
     // TODO: implement dispose
     tweetController.dispose();
     super.dispose();
+  }
+
+  void onPickImages() async {
+    images = await pickFiles();
+    setState(() {});
   }
 
   Widget build(BuildContext context) {
@@ -80,6 +90,17 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                         ),
                       ],
                     ),
+                    if (images.isNotEmpty)
+                      CarouselSlider(
+                          items: images.map((file) {
+                            return Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: Image.file(file));
+                          }).toList(),
+                          options: CarouselOptions(
+                              height: 400, enableInfiniteScroll: false)),
                   ],
                 ),
               ),
@@ -96,7 +117,7 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
             Padding(
               padding: const EdgeInsets.all(8.0).copyWith(left: 15, right: 15),
               child: GestureDetector(
-                onTap: () {},
+                onTap: onPickImages,
                 child: SvgPicture.asset(AssetsConstants.galleryIcon),
               ),
             ),
